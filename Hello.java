d129fd8aa8b5e8b1169483688b1bf64fa208e949
SereinR <98984783+SereinR@users.noreply.github.com>
2025-11-12 10:32:49 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!") // <-- missing semicolon to cause a compile error
+        System.out.println("Hello, Jenkins!");
     }
 }
